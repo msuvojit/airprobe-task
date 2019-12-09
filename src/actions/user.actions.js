73a9b62cf82b5ce8fa035c5d5960ae5f4b80e8bc
suvojit mondal <msuvojit@gmail.com>
2019-12-09 23:23:04 +0530

    remove unwanted package

--- src/actions/user.actions.js
+++ src/actions/user.actions.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 // import { setAlert } from "./alert";
 import {
     REGISTER_SUCCESS,
--- src/components/pages/user-register.jsx
+++ src/components/pages/user-register.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import axios from "axios";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {Redirect} from "react-router-dom";
